b3ebb4715fe0688c240a107f7749ee29b733af83
hanirnrh <nurrahmathaniff@gmail.com>
2025-02-03 10:04:23 +0700

    Robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,12 +4,8 @@ export default function robots(): MetadataRoute.Robots {
     return {
         rules: [
             {
-                userAgent: "Googlebot",
+                userAgent: ["Googlebot", "Applebot", "Bingbot"],
                 allow: ["/"],
-            },
-            {
-                userAgent: ["Applebot", "Bingbot"],
-                disallow: ["/"],
             },
         ],
         sitemap: "https://ecofix.id/sitemap.xml",
